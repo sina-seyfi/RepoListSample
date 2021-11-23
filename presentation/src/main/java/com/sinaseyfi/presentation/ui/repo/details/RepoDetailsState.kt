a43fd7fdbbf2fc3c25eca942bb7cc11bc9295f07
Sina Seyfi <sina.seyfi.1994@gmail.com>
2021-11-23 14:53:54 +0330

    Fixed a bug on repo details state.

--- presentation/src/main/java/com/sinaseyfi/presentation/ui/repo/details/RepoDetailsState.kt
+++ presentation/src/main/java/com/sinaseyfi/presentation/ui/repo/details/RepoDetailsState.kt
@@ -3,5 +3,6 @@ package com.sinaseyfi.presentation.ui.repo.details
 import com.sinaseyfi.presentation.ui.base.ViewState
 
 data class RepoDetailsState(
-    override val isLoading: Boolean = false
+    override val isLoading: Boolean = false,
+    val collaboratorsCanNotBeLoaded: Boolean = false
 ): ViewState
